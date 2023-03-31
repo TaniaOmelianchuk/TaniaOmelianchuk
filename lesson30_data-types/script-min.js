6497e6ba8f6b9d9746f5cbc054c46ae55d384ef1
TaniaOmelianchuk <taniaomelyanchuk@gmail.com>
2023-03-31 14:35:33 +0300

    added lesson30

---
+++ lesson30_data-types/script-min.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// let sum = (0.1 * 10 + 0.2 * 10) / 10;
+// alert(sum);
+
+// const num1 = "1";
+// const num2 = 2;
+// let sum = Number.parseInt(num1) + num2;
+// alert(sum);
+
+// const fileSizeMb = "820 Мб";
+// const usbSizeGb = prompt("Введіть обсяг флешки у ГБ");
+// let usbSizeMb = Number.parseInt(usbSizeGb) * 1024;
+// let totalNumOfFiles = usbSizeMb / Number.parseInt(fileSizeMb);
+// alert("Кількість можливих файлів розміром 820 Мб на флешці: " + Math.round(totalNumOfFiles));
---
+++ lesson30_data-types/script-normal.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const sumOfMoney = prompt("Вкажіть суму грошей яка у Вас є");
+// const chocolatePrice = prompt("Вкажіть ціну однієї шоколадки");
+// let numOfChocolates = Number.parseInt(sumOfMoney) / Number.parseInt(chocolatePrice);
+// let change = Number.parseInt(sumOfMoney) - (Number.parseInt(numOfChocolates) * Number.parseInt(chocolatePrice));
+// alert("Кількість шоколадок, які ви можете купити: " + Math.round(numOfChocolates) + ", та сума яка у Вас залишиться: " + change.toFixed(2) + " грн.");
+
+// const num = prompt("Введіть трьохзначне число");
+// if (isNaN(num) || num < 100 || num > 999) {
+//     alert("Введіть коректне значення!");
+// } else {
+//     const char1 = num % 10;
+//     const char2 = ((num % 100 ) - char1) / 10;
+//     const char3 = (num - (num % 100)) / 100;
+//     alert(`${char1}${char2}${char3}`);
+// }
