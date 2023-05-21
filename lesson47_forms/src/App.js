d13fec9b91780e2703373f184fa8ed7ab730a22d
TaniaOmelianchuk <taniaomelyanchuk@gmail.com>
2023-05-21 20:50:51 +0300

    added lesson47

---
+++ lesson47_forms/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import { Form1 } from './Form1/Form1';
+// import { Form2 } from "./Form2/Form2";
+
+function App() {
+  return (
+    <>
+      <Form1 />
+      {/* <Form2 /> */}
+    </>
+  );
+}
+
+export default App;
